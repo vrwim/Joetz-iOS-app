74024f1615ad1caafad09c527181f297a8344855
Wim Van Renterghem <vrwim@icloud.com>
2014-11-07 13:12:53 +0100

    Added Facebook framework

---
+++ iOS/JOETZ/Bridge-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridge-Header.h
+//  JOETZ
+//
+//  Created by Wim Van Renterghem on 11/7/14.
+//  Copyright (c) 2014 Groep 3D07. All rights reserved.
+//
+
+#ifndef JOETZ_Bridge_Header_h
+#define JOETZ_Bridge_Header_h
+
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
