e91061f628654c4fec4e0f3ba5ae6d4a3e372336
Jan-Vnh <jan.vanhulle05@gmail.com>
2014-11-26 13:18:11 +0100

    Core data global settings (viewType) + desc
    Zal nog worden verandert naar NSUserDefaults
    Veel beter en veel eenvoudiger (is echt bedoeld voor app settings)

---
+++ iOS/JOETZ/GlobalSettings.swift
@@ -0,0 +1,16 @@
+//
+//  GlobalSettings.swift
+//  JOETZ
+//
+//  Created by Jan Vanhulle on 26/11/14.
+//  Copyright (c) 2014 Groep 3D07. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class GlobalSettings: NSManagedObject {
+
+    @NSManaged var viewType: String
+
+}
--- iOS/JOETZ/StartViewController.swift
+++ iOS/JOETZ/StartViewController.swift
@@ -7,26 +7,44 @@
 //
 
 import UIKit
+import CoreData
 
 class StartViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         println("StartViewController did load")
-    }
-
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // checken op "kind" of "ouder" segue; "login" niets doen, laten overgaan
     }
     
     @IBAction func childButton(sender: UIButton) {
+        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        let context: NSManagedObjectContext = appDel.managedObjectContext!
+        
+        let ent = NSEntityDescription.entityForName("GlobalSettings", inManagedObjectContext: context)
+        
+        var globalSettings = GlobalSettings(entity: ent!, insertIntoManagedObjectContext: context)
+        globalSettings.viewType = "childView"
+        
+        context.save(nil)
+        
         let newTopViewController: UIViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ChildTripsNavVC") as UIViewController
         self.slidingViewController().topViewController = newTopViewController
         
     }
     
     @IBAction func parentButton(sender: UIButton) {
+        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        let context: NSManagedObjectContext = appDel.managedObjectContext!
+        
+        let ent = NSEntityDescription.entityForName("GlobalSettings", inManagedObjectContext: context)
+        
+        var globalSettings = GlobalSettings(entity: ent!, insertIntoManagedObjectContext: context)
+        globalSettings.viewType = "parentView"
+        
+        context.save(nil)
+        
         let newTopViewController: UIViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ParentTripsNavVC") as UIViewController
         self.slidingViewController().topViewController = newTopViewController
     }
@@ -46,6 +64,10 @@ class StartViewController: UIViewController {
         })*/
         
         self.slidingViewController().topViewController = newTopViewController
+        
+    }
+    
+    func addViewTypeSetting(viewType: String){
         
     }
 }
