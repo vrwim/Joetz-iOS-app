6983943f46d8946d2d9a830b3b0bd3dfb8075ce5
Wim Van Renterghem <vrwim@icloud.com>
2014-11-26 13:44:24 +0100

    Setup FBLoginView

--- iOS/JOETZ/AppDelegate.swift
+++ iOS/JOETZ/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        // FBLoginView() to load class before storyboard
+        // FBLoginView() // to load class before storyboard
         return true
     }
 
